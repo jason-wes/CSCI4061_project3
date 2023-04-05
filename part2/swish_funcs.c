a273ed2b3cfd3405ff35869979e0c6452e659f69
JStolzb <stolz050@umn.edu>
2023-04-05 13:55:26 -0500

    Passing all tests for part2 without using valgrind, valgrind errors though

--- part2/swish_funcs.c
+++ part2/swish_funcs.c
@@ -4,8 +4,7 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
-// may need to remove
-#include <string.h>
+
 #include "string_vector.h"
 #include "swish_funcs.h"
 
@@ -27,19 +26,49 @@
  */
 int run_piped_command(strvec_t *tokens, int *pipes, int n_pipes, int in_idx, int out_idx) {
     // TODO Complete this function's implementation
-    if (in_idx != -1) {
-        // need to EH dup2 and close
-        dup2(pipes[in_idx], STDIN_FILENO);
-        close(pipes[in_idx]);
-    }
-    if (out_idx != -1) {
-        // need to EH dup2 and close
-        dup2(pipes[out_idx], STDOUT_FILENO);
-        close(pipes[out_idx]);
-    }
 
-    // Need EH here
-    run_command(tokens);
+    pid_t child_pid = fork();
+
+        if (child_pid == -1) {
+            // fork failure
+            perror("fork");
+            return -1;
+        } else if (child_pid == 0) {
+            // child process
+            int dup2_fail = 0;
+            if (in_idx != -1) {
+                // need to EH dup2 and close
+                if (dup2(pipes[in_idx], STDIN_FILENO) == -1) {
+                    dup2_fail = 1;
+                    perror("dup2");
+                }
+            }
+            if (out_idx != -1) {
+                // need to EH dup2 and close
+                if (dup2(pipes[out_idx], STDOUT_FILENO) == -1) {
+                    dup2_fail = 1;
+                    perror("dup2");
+                }
+            }
+
+            int close_failure = 0;
+            for (int i = 0; i < n_pipes; i++) {
+                if (close(pipes[2*i])) {
+                    perror("close");
+                    close_failure = -1;
+                }
+                if (close(pipes[2*i + 1])) {
+                    perror("close");
+                    close_failure = 1;
+                }
+            }
+            
+            if (close_failure || dup2_fail) {
+                free(pipes);
+                exit(-1);
+            }
+            run_command(tokens);
+        } 
 
     return 0;
 }
@@ -69,142 +98,121 @@ int run_pipelined_commands(strvec_t *tokens) {
             return -1;
         }
     }
+    // create temp strvec obj
+    strvec_t temp_tokens;
+    if(strvec_init(&temp_tokens) == -1) {
+        printf("strvec_init error\n");
+        strvec_clear(&temp_tokens);
+        for (int i = 0; i < n; i++) {
+            close(pipe_fds[2*i]);
+            close(pipe_fds[2*i + 1]);
+        }
+        free(pipe_fds);
+        return -1;
+    }
 
-    // Create child process for each file
-    for (int i = 0; i < n+1; i++) {
-        pid_t child_pid = fork();
+    // loop through, taking the last command of the piped command out each time, until we have no more pipes
+    int last_pipe_index = 0;
+    for (int i = n; i >= 0; i--) {
+        // printf("here\n");
+        last_pipe_index = strvec_find_last(tokens, "|");
+        // printf("%d pipe location: %d\ntoken length: %d", i, last_pipe_index, tokens->length);
 
-        if (child_pid == -1) {
-            perror("fork");
-            for (int j = 0; j < n; j++) {
+        // put the desired slice into temp_tokens
+        if (strvec_slice(tokens, &temp_tokens, last_pipe_index+1, tokens->length) == -1) {
+            printf("strvec_slice error\n");
+            strvec_clear(&temp_tokens);
+            for (int j = 0; j < i; j++) {
                 close(pipe_fds[2*j]);
                 close(pipe_fds[2*j + 1]);
             }
             free(pipe_fds);
             return -1;
-        } else if (child_pid == 0) {
+        }
+        
+        // printf("here\n");
+        
+        // for (int j = 0; j<temp_tokens.length; j++) {
+        //     printf("%s ", temp_tokens.data[j]);
+        // }
+        // printf("\n");
 
-            // Close unused pipes
-            int failure = 0;
-            for (int j = 0; j < n; j++) {
-
-                // close necessary read ends
-                if (j != i-1 && close(pipe_fds[2*j]) == -1) {
-                    perror("close");
-                    failure = 1;
-                }
-
-                // close necessary write ends
-                if (j != i && close(pipe_fds[2*j + 1]) == -1) {
-                    perror("close");
-                    failure = 1;
-                }
-            }
-
-            // create temp strvec obj
-            strvec_t temp_tokens;
-            if(strvec_init(&temp_tokens) == -1) {
-                printf("strvec_init error\n");
-                strvec_clear(&temp_tokens);
-                failure = 1;
-            }
-
-            // find the correct slice in the string vector for the current child
-            int prev_pipe_index = 0;
-            int next_pipe_index = 0;
-            int num_occurrences = 0;
-            for (int j = 0; j < tokens->length; j++) {
-
-                if (strcmp(tokens->data[j], "|") == 0) {
-                    prev_pipe_index = next_pipe_index;
-                    next_pipe_index = j;
-                    num_occurrences++;
-                }
-
-                if (num_occurrences == i+1) {
-                    break;
-                }
-
-                if (j == tokens->length-1) {
-                    prev_pipe_index = next_pipe_index;
-                    next_pipe_index = tokens->length;
-                }
-            }
-
-            // This will exclude the pipe for string vector slicing
-            if (prev_pipe_index != 0) {
-                prev_pipe_index++;
-            }
-
-            // put the desired slice into temp_tokens
-            if (strvec_slice(tokens, &temp_tokens, prev_pipe_index, next_pipe_index)) {
-                printf("strvec_slice error\n");
-                strvec_clear(&temp_tokens);
-                failure = 1;
-            }
-
-            // error handling
-            if (failure) {
-                // close cur child read end
-                if (i != 0) {
-                    close(pipe_fds[2*(i-1)]);
-                }
-                // close cur child write end
-                if (i != n) {
-                    close(pipe_fds[2*i + 1]);
-                }
+        strvec_take(tokens, last_pipe_index);
+        if (i == n) {
+            // last piped command
+            if (run_piped_command(&temp_tokens, pipe_fds, n, 2*(i-1), -1)) {
                 free(pipe_fds);
-                exit(-1);
+                strvec_clear(&temp_tokens);
+
+                // need to close all pipes for failure
+                for (int j = 0; j < n; j++) {
+                    if (close(pipe_fds[2*j])) {
+                        perror("close");
+                    }
+                    if (close(pipe_fds[2*j + 1])) {
+                        perror("close");
+                    }
+                }
+
+                return -1;
             }
 
-            // for (int j = 0; j<temp_tokens.length; j++) {
-            //     printf("%s", temp_tokens.data[j]);
-            // }
-            // printf("\n");
-            // printf("number of tokens: %d\n", temp_tokens.length);
+        } else if (i == 0) {
+            // first piped command
+            if (run_piped_command(&temp_tokens, pipe_fds, n, -1, 2*i + 1)) {
+                free(pipe_fds);
+                strvec_clear(&temp_tokens);
 
-            if (i == 0) {
-                if (run_piped_command(&temp_tokens, pipe_fds, n, -1, 2*i + 1)) {
-                    free(pipe_fds);
-                    strvec_clear(&temp_tokens);
-                    exit(-1);
-                } else {
-                    free(pipe_fds);
-                    strvec_clear(&temp_tokens);
-                    exit(1);
+                // need to close all pipes for failure
+                for (int j = 0; j < n; j++) {
+                    if (close(pipe_fds[2*j])) {
+                        perror("close");
+                    }
+                    if (close(pipe_fds[2*j + 1])) {
+                        perror("close");
+                    }
                 }
-            } else if (i == n) {
-                if (run_piped_command(&temp_tokens, pipe_fds, n, 2*(i-1), -1)) {
-                    free(pipe_fds);
-                    strvec_clear(&temp_tokens);
-                    exit(-1);
-                } else {
-                    free(pipe_fds);
-                    strvec_clear(&temp_tokens);
-                    exit(1);
-                }
-            } else {
-                if (run_piped_command(&temp_tokens, pipe_fds, n, 2*(i-1), 2*i + 1)) {
-                    free(pipe_fds);
-                    strvec_clear(&temp_tokens);
-                    exit(-1);
-                } else {
-                    free(pipe_fds);
-                    strvec_clear(&temp_tokens);
-                    exit(1);
-                }
+
+                return -1;
             }
-            
-        } 
+
+        } else {
+            // interior piped commands
+            if (run_piped_command(&temp_tokens, pipe_fds, n, 2*(i-1), 2*i + 1)) {
+                free(pipe_fds);
+                strvec_clear(&temp_tokens);
+
+                // need to close all pipes for failure
+                for (int j = 0; j < n; j++) {
+                    if (close(pipe_fds[2*j])) {
+                        perror("close");
+                    }
+                    if (close(pipe_fds[2*j + 1])) {
+                        perror("close");
+                    }
+                }
+                return -1;
+
+            }
+
+        }
     }
-
+    strvec_clear(&temp_tokens);
+    // all pipes close except parent process at the end
+    for (int j = 0; j < n; j++) {
+        if (close(pipe_fds[2*j])) {
+            perror("close");
+        }
+        if (close(pipe_fds[2*j + 1])) {
+            perror("close");
+        }
+    }
     free(pipe_fds);
 
     // some wait logic with checking exit codes
-    // TODO figure out how to wait for all children to finish without causing timeout on automated testing
-    // for (int i = 0; i<n+1; i++) {
-    //     wait(NULL);
-    // }
+    for (int i = 0; i<n+1; i++) {
+        wait(NULL);
+    }
 
 
     return 0;
